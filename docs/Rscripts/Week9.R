c15c6b8a2783a9621396c882dad290ff7ca371ed
Charlotte Chang <chchang@pomona.edu>
2021-03-23 17:32:59 -0700

    Updating week 9 Rscript to include sections 1 and 2 of tutorial and making package calls visible

--- docs/Rscripts/Week9.R
+++ docs/Rscripts/Week9.R
@@ -5,11 +5,41 @@
 ### Load the ggplot2 package into the R workspace
 library(ggplot2)
 
+###========================================
+### Code for Sections 1 and 2
+###========================================
 ### Reading in the data
 phenoDF <- readr::read_tsv("https://raw.githubusercontent.com/charlottehchang/BIOL104PO/master/data/ButterflyPhenology.tsv")
 
 ### Display the first few rows of the data
 phenoDF
+
+### Building on the ggplot tutorial from Week 4
+  # Specifically, the scatterplot exercise from Section 2
+  # Link: https://charlottehchang.github.io/BIOL104PO/Week4.html
+p <- ggplot(phenoDF, aes(x=SpringTempC, y=DayOfYear))
+p <- p + geom_point() # adding on points plotted at the locations given by x and y above
+p <- p + labs(x="Mean spring temperature (°C)", y="Flight timing (day of year)") # changing the x and y-axis labels to be informative
+p <- p + theme_classic() # modify the style of the plot
+p # display the plot, p
+
+### Running the lm command
+butterflyModel <- lm(DayOfYear ~ SpringTempC, data=phenoDF)
+
+### Showing the coefficient estimates
+summary(butterflyModel)
+
+### Generating plot with line
+p <- ggplot(phenoDF, aes(x=SpringTempC, y=DayOfYear))
+p <- p + geom_point() # adding on points plotted at the locations given by x and y above
+p <- p + geom_smooth(method="lm", fill=NA)
+p <- p + labs(x="Mean spring temperature (°C)", y="Flight timing (day of year)") # changing the x and y-axis labels to be informative
+p <- p + theme_bw() # modify the style of the plot
+p # display the plot, p
+
+###========================================
+### Code for Section 3 (Gradescope Homework)
+###========================================
 
 ### Running the lm command
 summerButterflyModel <- lm(DayOfYear ~ ..., data=phenoDF) 
